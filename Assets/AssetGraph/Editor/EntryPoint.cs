f89c021caf43b546060e2a322b55c9bcfba2fc34
sassembla <toru.inoue@kissaki.tv>
2015-07-05 23:58:05 +0900

    add namespace

--- Assets/AssetGraph/Editor/EntryPoint.cs
+++ Assets/AssetGraph/Editor/EntryPoint.cs
@@ -2,14 +2,13 @@
 using UnityEditor;
 
 using System.Collections;
-using AssetGraph;
 
 
 [InitializeOnLoad]
 public class EntryPoint {
 	static EntryPoint () {
 		Debug.Log("仮のエントリーポイント、適当にGUIを出さなくても動くテスト起動用として機能させる。");
-		var a = new AssetGraph();
+		var a = new AssetGraph.AssetGraph();
 		a.InitializeGraph();
 		Debug.Log("test done");
 	}
