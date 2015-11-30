06f22742e4deb51c7048d97ebfbf840c963c5edf
sassembla <toru.inoue@kissaki.tv>
2015-11-30 23:47:06 +0900

    add tests(last 5_3_rev is failed.)

--- Assets/AssetGraphTest/Editor/Tests/Tests5.cs
+++ Assets/AssetGraphTest/Editor/Tests/Tests5.cs
@@ -87,4 +87,232 @@ public partial class Test {
 		if (after_iOSAssetGUID != before_iOSAssetGUID) Debug.LogError("failed to cache after_iOSAssetGUID:" + after_iOSAssetGUID + " before_iOSAssetGUID:" + before_iOSAssetGUID);
 	}
 
+	public void _5_1_PlatformChanging_rev () {
+
+		GraphStackController.CleanCache();
+		GraphStackController.CleanSetting();
+		
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.StandaloneOSXIntel);
+
+		var dataPath = Path.Combine(Application.dataPath, "AssetGraphTest/Editor/TestData");
+		var graphDataPath = Path.Combine(dataPath, "_5_0_PlatformChanging.json");
+		
+		// load
+		var dataStr = string.Empty;
+		
+		using (var sr = new StreamReader(graphDataPath)) {
+			dataStr = sr.ReadToEnd();
+		}
+		var graphDict = Json.Deserialize(dataStr) as Dictionary<string, object>;
+		
+		Action setup = () => {
+			var EndpointNodeIdsAndNodeDatasAndConnectionDatas = GraphStackController.SerializeNodeRoute(graphDict, string.Empty);
+		
+			var endpointNodeIds = EndpointNodeIdsAndNodeDatasAndConnectionDatas.endpointNodeIds;
+			var nodeDatas = EndpointNodeIdsAndNodeDatasAndConnectionDatas.nodeDatas;
+			var connectionDatas = EndpointNodeIdsAndNodeDatasAndConnectionDatas.connectionDatas;
+
+			var cacheDict = new Dictionary<string, List<string>>();
+			var resultDict = new Dictionary<string, Dictionary<string, List<InternalAssetData>>>();
+
+			foreach (var endNodeId in endpointNodeIds) {
+				GraphStackController.SetupSerializedRoute(endNodeId, nodeDatas, connectionDatas, resultDict, cacheDict, string.Empty);
+			}
+		};
+
+		Action run = () => {
+			var EndpointNodeIdsAndNodeDatasAndConnectionDatas = GraphStackController.SerializeNodeRoute(graphDict, string.Empty);
+		
+			var endpointNodeIds = EndpointNodeIdsAndNodeDatasAndConnectionDatas.endpointNodeIds;
+			var nodeDatas = EndpointNodeIdsAndNodeDatasAndConnectionDatas.nodeDatas;
+			var connectionDatas = EndpointNodeIdsAndNodeDatasAndConnectionDatas.connectionDatas;
+
+			var cacheDict = new Dictionary<string, List<string>>();
+			var resultDict = new Dictionary<string, Dictionary<string, List<InternalAssetData>>>();
+
+			foreach (var endNodeId in endpointNodeIds) {
+				GraphStackController.RunSerializedRoute(endNodeId, nodeDatas, connectionDatas, resultDict, cacheDict, string.Empty);
+			}
+		};
+
+		setup();
+		run();
+
+		// cache generated.
+		uint before_iOSAssetGUID;
+		BuildPipeline.GetCRCForAssetBundle("Assets/AssetGraph/Cache/BundleBuilt/c464cf25-acf0-4678-aae3-d598e44dcc60/StandaloneOSXIntel/chara_0.assetbundle", out before_iOSAssetGUID);
+
+		// change platform.
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.iOS);
+		setup();
+		run();
+
+
+		// change platform again.
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.StandaloneOSXIntel);
+
+		// should cache.
+
+		setup();
+		run();
+		// the GUID of assetBundle for iOS platform should be keep. = cached.
+
+		uint after_iOSAssetGUID;
+		BuildPipeline.GetCRCForAssetBundle("Assets/AssetGraph/Cache/BundleBuilt/c464cf25-acf0-4678-aae3-d598e44dcc60/StandaloneOSXIntel/chara_0.assetbundle", out after_iOSAssetGUID);
+
+		if (after_iOSAssetGUID != before_iOSAssetGUID) Debug.LogError("failed to cache after_iOSAssetGUID:" + after_iOSAssetGUID + " before_iOSAssetGUID:" + before_iOSAssetGUID);
+	}
+
+	public void _5_2_PlatformChanging () {
+
+		GraphStackController.CleanCache();
+		GraphStackController.CleanSetting();
+		
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.iOS);
+
+		var dataPath = Path.Combine(Application.dataPath, "AssetGraphTest/Editor/TestData");
+		var graphDataPath = Path.Combine(dataPath, "_5_2_PlatformChanging.json");
+		
+		// load
+		var dataStr = string.Empty;
+		
+		using (var sr = new StreamReader(graphDataPath)) {
+			dataStr = sr.ReadToEnd();
+		}
+		var graphDict = Json.Deserialize(dataStr) as Dictionary<string, object>;
+		
+		Action setup = () => {
+			var EndpointNodeIdsAndNodeDatasAndConnectionDatas = GraphStackController.SerializeNodeRoute(graphDict, string.Empty);
+		
+			var endpointNodeIds = EndpointNodeIdsAndNodeDatasAndConnectionDatas.endpointNodeIds;
+			var nodeDatas = EndpointNodeIdsAndNodeDatasAndConnectionDatas.nodeDatas;
+			var connectionDatas = EndpointNodeIdsAndNodeDatasAndConnectionDatas.connectionDatas;
+
+			var cacheDict = new Dictionary<string, List<string>>();
+			var resultDict = new Dictionary<string, Dictionary<string, List<InternalAssetData>>>();
+
+			foreach (var endNodeId in endpointNodeIds) {
+				GraphStackController.SetupSerializedRoute(endNodeId, nodeDatas, connectionDatas, resultDict, cacheDict, string.Empty);
+			}
+		};
+
+		Action run = () => {
+			var EndpointNodeIdsAndNodeDatasAndConnectionDatas = GraphStackController.SerializeNodeRoute(graphDict, string.Empty);
+		
+			var endpointNodeIds = EndpointNodeIdsAndNodeDatasAndConnectionDatas.endpointNodeIds;
+			var nodeDatas = EndpointNodeIdsAndNodeDatasAndConnectionDatas.nodeDatas;
+			var connectionDatas = EndpointNodeIdsAndNodeDatasAndConnectionDatas.connectionDatas;
+
+			var cacheDict = new Dictionary<string, List<string>>();
+			var resultDict = new Dictionary<string, Dictionary<string, List<InternalAssetData>>>();
+
+			foreach (var endNodeId in endpointNodeIds) {
+				GraphStackController.RunSerializedRoute(endNodeId, nodeDatas, connectionDatas, resultDict, cacheDict, string.Empty);
+			}
+		};
+
+		setup();
+		run();
+
+		// cache generated.
+		uint before_iOSAssetGUID;
+		BuildPipeline.GetCRCForAssetBundle("Assets/AssetGraph/Cache/BundleBuilt/05ad002c-5e3a-46e3-97e3-01543516c728/iOS/chara_0.assetbundle", out before_iOSAssetGUID);
+
+		// change platform.
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.StandaloneOSXIntel);
+		setup();
+		run();
+
+
+		// change platform again.
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.iOS);
+
+		// should cache.
+
+		setup();
+		run();
+		// the GUID of assetBundle for iOS platform should be keep. = cached.
+
+		uint after_iOSAssetGUID;
+		BuildPipeline.GetCRCForAssetBundle("Assets/AssetGraph/Cache/BundleBuilt/05ad002c-5e3a-46e3-97e3-01543516c728/iOS/chara_0.assetbundle", out after_iOSAssetGUID);
+
+		if (after_iOSAssetGUID != before_iOSAssetGUID) Debug.LogError("failed to cache after_iOSAssetGUID:" + after_iOSAssetGUID + " before_iOSAssetGUID:" + before_iOSAssetGUID);
+	}
+
+	public void _5_3_PlatformChanging_rev () {
+
+		GraphStackController.CleanCache();
+		GraphStackController.CleanSetting();
+		
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.StandaloneOSXIntel);
+
+		var dataPath = Path.Combine(Application.dataPath, "AssetGraphTest/Editor/TestData");
+		var graphDataPath = Path.Combine(dataPath, "_5_2_PlatformChanging.json");
+		
+		// load
+		var dataStr = string.Empty;
+		
+		using (var sr = new StreamReader(graphDataPath)) {
+			dataStr = sr.ReadToEnd();
+		}
+		var graphDict = Json.Deserialize(dataStr) as Dictionary<string, object>;
+		
+		Action setup = () => {
+			var EndpointNodeIdsAndNodeDatasAndConnectionDatas = GraphStackController.SerializeNodeRoute(graphDict, string.Empty);
+		
+			var endpointNodeIds = EndpointNodeIdsAndNodeDatasAndConnectionDatas.endpointNodeIds;
+			var nodeDatas = EndpointNodeIdsAndNodeDatasAndConnectionDatas.nodeDatas;
+			var connectionDatas = EndpointNodeIdsAndNodeDatasAndConnectionDatas.connectionDatas;
+
+			var cacheDict = new Dictionary<string, List<string>>();
+			var resultDict = new Dictionary<string, Dictionary<string, List<InternalAssetData>>>();
+
+			foreach (var endNodeId in endpointNodeIds) {
+				GraphStackController.SetupSerializedRoute(endNodeId, nodeDatas, connectionDatas, resultDict, cacheDict, string.Empty);
+			}
+		};
+
+		Action run = () => {
+			var EndpointNodeIdsAndNodeDatasAndConnectionDatas = GraphStackController.SerializeNodeRoute(graphDict, string.Empty);
+		
+			var endpointNodeIds = EndpointNodeIdsAndNodeDatasAndConnectionDatas.endpointNodeIds;
+			var nodeDatas = EndpointNodeIdsAndNodeDatasAndConnectionDatas.nodeDatas;
+			var connectionDatas = EndpointNodeIdsAndNodeDatasAndConnectionDatas.connectionDatas;
+
+			var cacheDict = new Dictionary<string, List<string>>();
+			var resultDict = new Dictionary<string, Dictionary<string, List<InternalAssetData>>>();
+
+			foreach (var endNodeId in endpointNodeIds) {
+				GraphStackController.RunSerializedRoute(endNodeId, nodeDatas, connectionDatas, resultDict, cacheDict, string.Empty);
+			}
+		};
+
+		setup();
+		run();
+
+		// cache generated.
+		uint before_iOSAssetGUID;
+		BuildPipeline.GetCRCForAssetBundle("Assets/AssetGraph/Cache/BundleBuilt/05ad002c-5e3a-46e3-97e3-01543516c728/StandaloneOSXIntel/chara_0.assetbundle", out before_iOSAssetGUID);
+
+		// change platform.
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.iOS);
+		setup();
+		run();
+
+		
+		// // change platform again.
+		// EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.StandaloneOSXIntel);
+
+		// // should cache.
+
+		// setup();
+		// run();
+		// // the GUID of assetBundle for iOS platform should be keep. = cached.
+
+		// uint after_iOSAssetGUID;
+		// BuildPipeline.GetCRCForAssetBundle("Assets/AssetGraph/Cache/BundleBuilt/05ad002c-5e3a-46e3-97e3-01543516c728/StandaloneOSXIntel/chara_0.assetbundle", out after_iOSAssetGUID);
+
+		// if (after_iOSAssetGUID != before_iOSAssetGUID) Debug.LogError("failed to cache after_iOSAssetGUID:" + after_iOSAssetGUID + " before_iOSAssetGUID:" + before_iOSAssetGUID);
+	}
+
 }
