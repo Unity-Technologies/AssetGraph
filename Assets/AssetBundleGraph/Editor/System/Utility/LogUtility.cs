5ac573f78a50c3651b92fe4af5ecf8a51427fa87
Hiroki Omae <hiroki@unity3d.com>
2017-02-24 15:47:26 +0900

    Support Unity 2017.1a1

--- Assets/AssetBundleGraph/Editor/System/Utility/LogUtility.cs
+++ Assets/AssetBundleGraph/Editor/System/Utility/LogUtility.cs
@@ -27,7 +27,11 @@ namespace UnityEngine.AssetBundles.GraphTool {
 		public static Logger Logger {
 			get {
 				if(s_logger == null) {
+					#if UNITY_2017_1_OR_NEWER
+					s_logger = new Logger(Debug.unityLogger.logHandler);
+					#else
 					s_logger = new Logger(Debug.logger.logHandler);
+					#endif
 				}
 
 				return s_logger;
