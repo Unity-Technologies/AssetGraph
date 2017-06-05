e6089463fdfeb3767122f5f83d89d54169e71824
Hiroki Omae <hiroki@unity3d.com>
2017-06-05 16:10:39 +0900

    Support Project relative path

--- UnityEngine.AssetBundleGraph/Editor/GUI/ProjectSettingsWindow.cs
+++ UnityEngine.AssetBundleGraph/Editor/GUI/ProjectSettingsWindow.cs
@@ -37,6 +37,35 @@ namespace UnityEngine.AssetBundles.GraphTool {
 		public void OnDisable() {
 		}
 
+        public string DrawFolderSelector(string label, 
+            string dialogTitle, 
+            string currentDirPath, 
+            string directoryOpenPath, 
+            Func<string, string> onValidFolderSelected = null) 
+        {
+            string newDirPath;
+            using(new EditorGUILayout.HorizontalScope()) {
+                if (string.IsNullOrEmpty (label)) {
+                    newDirPath = EditorGUILayout.TextField(currentDirPath);
+                } else {
+                    newDirPath = EditorGUILayout.TextField(label, currentDirPath);
+                }
+
+                if(GUILayout.Button("Select", GUILayout.Width(50f))) {
+                    var folderSelected = 
+                        EditorUtility.OpenFolderPanel(dialogTitle, directoryOpenPath, "");
+                    if(!string.IsNullOrEmpty(folderSelected)) {
+                        if (onValidFolderSelected != null) {
+                            newDirPath = onValidFolderSelected (folderSelected);
+                        } else {
+                            newDirPath = folderSelected;
+                        }
+                    }
+                }
+            }
+            return newDirPath;
+        }
+
 		public void OnGUI () {
 
 			using (new EditorGUILayout.VerticalScope()) {
@@ -46,9 +75,45 @@ namespace UnityEngine.AssetBundles.GraphTool {
 
                 string cacheDir = Model.Settings.UserSettings.AssetBundleBuildCacheDir;
 
-                var newCacheDir = EditorGUILayout.TextField ("AB Cache Dir", cacheDir);
-                if (newCacheDir != cacheDir) {
-                    Model.Settings.UserSettings.AssetBundleBuildCacheDir = newCacheDir;
+                using (new EditorGUILayout.HorizontalScope ()) {                    
+                    var newCacheDir = DrawFolderSelector ("Bundle Cache Directory", "Select Cache Folder", 
+                        cacheDir,
+                        Application.dataPath + "/../",
+                        (string folderSelected) => {
+                            var projectPath = Directory.GetParent(Application.dataPath).ToString();
+
+                            if(projectPath == folderSelected) {
+                                folderSelected = string.Empty;
+                            } else {
+                                var index = folderSelected.IndexOf(projectPath);
+                                if(index >= 0 ) {
+                                    folderSelected = folderSelected.Substring(projectPath.Length + index);
+                                    if(folderSelected.IndexOf('/') == 0) {
+                                        folderSelected = folderSelected.Substring(1);
+                                    }
+                                }
+                            }
+                            return folderSelected;
+                        }
+                    );
+                    if (newCacheDir != cacheDir) {
+                        Model.Settings.UserSettings.AssetBundleBuildCacheDir = newCacheDir;
+                    }
+                }
+
+                using (new EditorGUI.DisabledScope (!Directory.Exists (cacheDir))) 
+                {
+                    using (new EditorGUILayout.HorizontalScope ()) {
+                        GUILayout.FlexibleSpace ();
+                        #if UNITY_EDITOR_OSX
+                        string buttonName = "Reveal in Finder";
+                        #else
+                        string buttonName = "Show in Explorer";
+                        #endif
+                        if (GUILayout.Button (buttonName)) {
+                            EditorUtility.RevealInFinder (cacheDir);
+                        }
+                    }
                 }
 			}
 		}
