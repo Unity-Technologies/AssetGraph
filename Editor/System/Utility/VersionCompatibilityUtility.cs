96983068d39b43f9facaa2dc52dd410df31ca01e
Hiroki Omae <hiroki@unity3d.com>
2018-07-20 12:27:54 +0900

    Support assembly migration of classes

--- Editor/System/Utility/VersionCompatibilityUtility.cs
+++ Editor/System/Utility/VersionCompatibilityUtility.cs
@@ -1,7 +1,7 @@
-﻿using UnityEngine;
-using UnityEditor;
-using System;
-using System.Collections;
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
 
 namespace Unity.AssetGraph {
 
@@ -13,8 +13,29 @@ namespace Unity.AssetGraph {
                 return className;
             }
             
-            // v1.3 -> v1.4
-            return className.Replace("UnityEngine.AssetBundles.GraphTool.", "Unity.AssetGraph.");
+            if (!className.StartsWith("Unity.AssetGraph."))
+            {
+                className = className
+                    .Replace("UnityEngine.AssetBundles.GraphTool.", "Unity.AssetGraph.") // v1.3 -> 1.5
+                    .Replace("UnityEngine.AssetGraph.", "Unity.AssetGraph."); // v1.4 -> 1.5
+            }
+
+            var typeGetTest = Type.GetType(className);
+            if (null == typeGetTest)
+            {
+                var fullname = className.Split(',')[0];
+                foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies()) {
+                    var matchingType = assembly.GetTypes().FirstOrDefault(t => t.FullName == fullname);
+                    if (matchingType != null)
+                    {
+                        return matchingType.AssemblyQualifiedName;
+                    }
+                }
+                
+                LogUtility.Logger.LogFormat(LogType.Error, "[VersionCompatibilityUtility] Type not found for class: {0}.", className );						
+            }
+
+            return className;
         }
     }
 }
