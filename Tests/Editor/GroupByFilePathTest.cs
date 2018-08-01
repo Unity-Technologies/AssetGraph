a80ad806330e6bb668d1a3c970da08dbbfdf045f
Hiroki Omae <hiroki@unity3d.com>
2018-08-01 16:45:37 +0900

    Implement GroupBy tests

--- Tests/Editor/GroupByFilePathTest.cs
+++ Tests/Editor/GroupByFilePathTest.cs
@@ -6,13 +6,21 @@ internal class GroupByFilePathTest : AssetGraphEditorBaseTest
 {
 	protected override void CreateResourcesForTests()
 	{
-		CreateTestPrefab("c0001/v0001", "body", PrimitiveType.Cube);
-		CreateTestPrefab("c0001/v0001", "head01", PrimitiveType.Cube);
-		CreateTestPrefab("c0001/v0002", "body", PrimitiveType.Cube);
-		CreateTestPrefab("c0001/v0002", "head01", PrimitiveType.Cube);
-		CreateTestPrefab("c0001/v0003", "body", PrimitiveType.Cube);
-		CreateTestPrefab("c0001/v0003", "head01", PrimitiveType.Cube);
-		CreateTestPrefab("c0001/v0003", "head02", PrimitiveType.Cube);
+		CreateTestPrefab("c0001/v0001/", "body", PrimitiveType.Cube);
+		CreateTestPrefab("c0001/v0001/", "head01", PrimitiveType.Cube);
+		CreateTestPrefab("c0001/v0002/", "body", PrimitiveType.Cube);
+		CreateTestPrefab("c0001/v0002/", "head01", PrimitiveType.Cube);
+		CreateTestPrefab("c0001/v0003/", "body", PrimitiveType.Cube);
+		CreateTestPrefab("c0001/v0003/", "head01", PrimitiveType.Cube);
+		CreateTestPrefab("c0001/v0003/", "head02", PrimitiveType.Cube);
+
+		CreateTestPrefab("c1834/v0001/", "body", PrimitiveType.Cube);
+		CreateTestPrefab("c1834/v0001/", "head01", PrimitiveType.Cube);
+		CreateTestPrefab("c1834/v0001/", "head02", PrimitiveType.Cube);
+		CreateTestPrefab("c1834/v0001/", "weapon", PrimitiveType.Cube);
+		CreateTestPrefab("c1834/v0002/", "body", PrimitiveType.Cube);
+		CreateTestPrefab("c1834/v0002/", "head01", PrimitiveType.Cube);
+		
 		CreateTestPrefab("boss/", "body", PrimitiveType.Cube);
 		CreateTestPrefab("boss/", "head01", PrimitiveType.Cube);
 		CreateTestPrefab("boss/", "weapon", PrimitiveType.Cube);
