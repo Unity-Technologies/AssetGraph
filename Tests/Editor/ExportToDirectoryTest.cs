a396a5db54b20de453058652a2f0b1ec68dbb6a4
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-11-20 11:59:26 +0900

    Removing sample tests

--- Tests/Editor/ExportToDirectoryTest.cs
+++ Tests/Editor/ExportToDirectoryTest.cs
@@ -12,21 +12,4 @@ internal class ExportToDirectoryTest : AssetGraphEditorBaseTest
 		CreateTestPrefab("Bad/", "prefab04", PrimitiveType.Cube);
 	}
 
-	[Test]
-	public void SampleTest()
-	{
-		var result = AssertGraphExecuteWithIssue();
-		
-		foreach (var e in result.Issues)
-		{
-			Assert.AreEqual(e.Node.Operation.ClassName, typeof(AssertUnwantedAssetsInBundle).AssemblyQualifiedName);
-			Assert.True(e.Reason.Contains("/Bad/"));
-		}
-	}
-	
-	[Test]
-	public void SampleTestNoIssue()
-	{
-		AssertGraphExecuteWithNoIssue();
-	}
 }
