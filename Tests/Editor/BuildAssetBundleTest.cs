51f647e75dac665c42a54312fe27f0174468387d
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2019-12-05 15:33:46 +0900

    feat: add Yamato settings and a test project (#31) (#32)
    * Add initial Yamato settings
    * Add empty project for testing the package directly
    * Modify gitignore to ignore test project's temp files
    * Add npmignore to omit package-unrelated files from being added to the package
    * Add a dummy package.json in the test project.
    * Create symbolic links in the test project, so that it refers to the actual package
    * Ignore autogenerated Visual Studio files
    * Add missing meta files
    * Create a symbolic link to Tests
    * Removing sample tests
    * Fixed Failed LastImport test. Removed Experimental PrefabBuilder Tests.
    * Commenting out LastImportedItemsTest.TestLastImportedItems

--- Tests/Editor/BuildAssetBundleTest.cs
+++ Tests/Editor/BuildAssetBundleTest.cs
@@ -13,21 +13,4 @@ internal class BuildAssetBundleTest  : AssetGraphEditorBaseTest
 		CreateTestPrefab("Bad/", "prefab04", PrimitiveType.Cube);
 	}
 
-	[Test]
-	public void SampleTest()
-	{
-		var result = AssertGraphExecuteWithIssue();
-		
-		foreach (var e in result.Issues)
-		{
-			Assert.AreEqual(e.Node.Operation.ClassName, typeof(AssertUnwantedAssetsInBundle).AssemblyQualifiedName);
-			Assert.True(e.Reason.Contains("/Bad/"));
-		}
-	}
-	
-	[Test]
-	public void SampleTestNoIssue()
-	{
-		AssertGraphExecuteWithNoIssue();
-	}
 }
